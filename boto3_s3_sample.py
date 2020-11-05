e7ca5a560540d7478f39351e4553c3ac6149583f
allen-dev-2020 <73958814+allen-dev-2020@users.noreply.github.com>
2020-11-05 20:18:30 +0000

    Update boto3_s3_sample.py

--- boto3_s3_sample.py
+++ boto3_s3_sample.py
@@ -1,8 +1,8 @@
 #############################
 #Sample for S3 bucket access#
 #############################
-mykey = "key"
-mysecretkey = "secret"
+mykey = "AKIAQH7JYYNITD5K2FFA"
+mysecretkey = "+aFPzML3TXA7TF7tzpqQZ0wqPeRwv92vqk4JuQlm"
 
 s3 = boto3.resource(
     service_name='s3',
